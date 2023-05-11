4163c64847c40a457ec69920a70bffa7253f83af
xoliu1 <xoliu6@gmail.com>
2023-05-11 12:51:54 +0800

    0511.01

--- learn/src/DP/力扣基础题目/tmp.java
+++ learn/src/DP/力扣基础题目/tmp.java
@@ -296,8 +296,27 @@ class Solution {
      */
 
     /**72. 编辑距离*/
-    public int minDistance(String word1, String word2) {
-        int
+    public int minDistance(String s1, String s2) {
+        int len1 = s1.length(), len2 = s2.length();
+        int[][] dp = new int[len1 + 1][len2 + 1];
+        //dp[i][j]表示s1[0,i] 和 s2[0,j] 最少操作数
+        //初始化，边界情况为另一个子串的长度
+        for (int i = 0; i <= len1; i++) {
+            dp[i][0] = i;
+        }
+        for (int i = 0; i <= len2; i++) {
+            dp[0][i] = i;
+        }
+        for (int i = 1; i <= len1; ++i) {
+            for (int j = 1; j <= len2; ++j) {
+                if (s1.charAt(i - 1) == s2.codePointAt(j - 1)){
+                    dp[i][j] = dp[i - 1][j - 1];
+                }else {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                }
+            }
+        }
+        return  dp[len1][len2];
     }
 
 }
