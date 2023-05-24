649e880eb821463f803241124749ae73f41770be
xoliu1 <xoliu6@gmail.com>
2023-05-24 12:51:18 +0800

    0524.01

--- learn/src/leetcode/solution.java
+++ learn/src/leetcode/solution.java
@@ -585,6 +585,34 @@ class Solution {
         }
     }
 
+    /**695. 岛屿的最大面积*/
+    public int maxAreaOfIsland(int[][] grid) {
+        if (grid.length == 0 || grid[0].length == 0){
+            return 0;
+        }
+        int res = 0;
+        for (int i = 0; i < grid.length; ++i) {
+            for (int j = 0; j < grid[0].length; ++j) {
+                if(grid[i][j] == 1){
+                    res = Math.max(dfsIsland(grid, i, j), res);
+                }
+            }
+        }
+        return res;
+    }
+    public int dfsIsland(int[][] grid, int i, int j){
+        if (i < 0 || i > grid.length - 1 || j < 0 || j > grid[0].length - 1 || grid[i][j] == 0){
+            return 0;
+        }
+        int ans = 1;
+        grid[i][j] = 0;
+        ans += dfsIsland(grid, i - 1, j);
+        ans += dfsIsland(grid, i + 1, j);
+        ans += dfsIsland(grid, i, j - 1);
+        ans += dfsIsland(grid, i, j + 1);
+        return ans;
+    }
+
 
 
 
